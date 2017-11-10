42d88d54b799ada76a602d669ea559e141126cc3
thomp <thomp@DESKTOP-TS3ACVK>
2017-11-09 21:29:48 -0500

    Updated code to break it up into smaller chunks

--- src/Character.java
+++ src/Character.java
@@ -1,6 +1,8 @@
+
 import java.util.Random;
 
 public class Character {
+
    String name;
    String race;
    int coins;
@@ -8,38 +10,56 @@ public class Character {
    int maxDrinks;
    int gameSkill;
 
-public Character(String a, String b){
-   name = a;
-   race = b;
-// Give character 2-30 coins to start with
-         Random rand = new Random();
-         int startCoins = rand.nextInt(10) + 20;
-         coins = startCoins;
-//Assign a dance skill number based on race    
+   public Character(String a, String b) {
+      name = a;
+      race = b;
+      // Give character 10-30 coins to start with
+      coins = startCoins();
+      //Assign a dance skill number based on race    
+      danceSkill = startDanceSkill();
+      // Give character max drink number
+      maxDrinks = startMaxDrinks();
+   }
+
+   public void printStats() {
+      System.out.println("Name: " + name);
+      System.out.println("Race: " + race);
+      System.out.println("Coins: " + coins);
+      // System.out.println("Dance Skills: " + danceSkill);
+      System.out.println("Max Drinks: " + maxDrinks);
+   }
+
+   private int startCoins() {
+      Random rand = new Random();
+      int startCoins = rand.nextInt(10) + 20;
+      return startCoins;
+   }
+
+   private int startMaxDrinks() {
+      Random rand = new Random();
+      int startMaxDrinks = rand.nextInt(10) + 10;
+      return startMaxDrinks;
+   }
+
+   private int startDanceSkill() {
          int startDanceSkill;
-         if (race.equalsIgnoreCase("Elf")){startDanceSkill = 80;}
-         else if (race.equalsIgnoreCase("Dwarf")){startDanceSkill = 50;}
-         else if (race.equalsIgnoreCase("Human")){startDanceSkill = 60;}
-         else if (race.equalsIgnoreCase("Wizard")){startDanceSkill = 60;}
-         else if (race.equalsIgnoreCase("Hobbit")){startDanceSkill = 85;}
-         else {startDanceSkill = 0;}   
-   danceSkill = startDanceSkill;   
- // Give character max drink number
-         Random randDrink = new Random();
-         int startMaxDrinks = randDrink.nextInt(10) + 10;
-         maxDrinks = startMaxDrinks;
- }
-
-public void printStats() {
-System.out.println("Name: " + name);
-System.out.println("Race: " + race);
-System.out.println("Coins: " + coins);
-// System.out.println("Dance Skills: " + danceSkill);
-System.out.println("Max Drinks: " + maxDrinks);
+      if (race.equalsIgnoreCase("Elf")) {
+         startDanceSkill = 80;
+      } else if (race.equalsIgnoreCase("Dwarf")) {
+         startDanceSkill = 50;
+      } else if (race.equalsIgnoreCase("Human")) {
+         startDanceSkill = 60;
+      } else if (race.equalsIgnoreCase("Wizard")) {
+         startDanceSkill = 60;
+      } else if (race.equalsIgnoreCase("Hobbit")) {
+         startDanceSkill = 85;
+      } else {
+         startDanceSkill = 0;
+      }
+      return startDanceSkill;
 }
-
-public void addCoin(int add) {
-coins = coins + add;
-}
+   public void addCoin(int add) {
+      coins = coins + add;
+   }
 
 }
---
+++ src/Game.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+public class Game {
+public String characterName;
+public String characterRace;
+
+public void playGame(){
+startGame();
+// Instantiate Character class
+Character currentCharacter = new Character(characterName, characterRace);       
+   boolean continuePlay;
+ do {
+   currentCharacter.printStats();
+   System.out.println("What would you like to do now? Drinking Game Or Quit");
+   Scanner scanInPlay = new Scanner(System.in);
+   String nextMove = scanInPlay.nextLine();
+   if (nextMove.equalsIgnoreCase("Drinking Game"))
+           {
+           DrinkingGame currentDrinkingGame = new DrinkingGame(currentCharacter.maxDrinks);
+           if (currentDrinkingGame.drink()) currentCharacter.addCoin(5);
+           }
+   continuePlay = (!nextMove.equalsIgnoreCase("Quit"));
+ } while (continuePlay);
+
+}
+
+public void startGame() {
+             System.out.println("Welcome, traveler, to the Green Dragon Inn! What's your name?");
+       Scanner scanIn = new Scanner(System.in);
+       characterName = scanIn.nextLine();
+       boolean invalidRace;
+       System.out.printf("Come on in, %s. ", characterName);
+// Ask for race - only allow elf, human, hobbit, wizard, or dwarf
+       do {
+       System.out.printf("Pardon me for asking, but what race are you? %n", characterName);
+       characterRace = scanIn.nextLine();
+       invalidRace = (!characterRace.equalsIgnoreCase("elf") && !characterRace.equalsIgnoreCase("human") && !characterRace.equalsIgnoreCase("hobbit") && !characterRace.equalsIgnoreCase("wizard") && !characterRace.equalsIgnoreCase("dwarf"));
+        if (invalidRace) {
+          System.out.printf("Never heard of that. ");
+        }
+        else {
+                    
+       System.out.printf("'Tis a windy night out! Step on in by the fireplace, %s. We're always happy to welcome a friendly %s to our company!%n", characterName, characterRace);
+        }
+       } while (invalidRace);
+}
+
+
+}
